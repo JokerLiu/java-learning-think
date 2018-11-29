8870721365d27fed761d7a93c8e882f5c5e34c19
Joker <liuyuelin0428@qq.com>
2018-11-29 20:22:17 +0800

    update 使用spring AMQPAdmin对RabbitMQ ExchangeQueue进行操作

--- src/main/java/com/mq/rabbit/quickstart/Producer.java
+++ src/main/java/com/mq/rabbit/quickstart/Producer.java
@@ -3,6 +3,10 @@ package com.mq.rabbit.quickstart;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.lang.Console;
 import com.mq.rabbit.RabbitMQConfig;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +24,9 @@ public class Producer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private AmqpAdmin amqpAdmin;// 用来创建Exchange,Queue,routing key,以及对应的绑定管
+
     @GetMapping("/sendMessage")
     public Object sendMessage() {
         new Thread(() -> {
@@ -36,6 +43,26 @@ public class Producer {
     public String sendMessageByWrite(@RequestParam("writeStr") String writeStr) {
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY, writeStr);
         return "ok";
+    }
+
+    @GetMapping("/createExchange")
+    public String createExchange(@RequestParam("exchange") String exchange) {
+        //amqpAdmin.deleteExchange(exchange);
+        //Exchange接口有四个实现子类。DirectExchange,FanoutExchange,TopicExchange,CustomExchange
+        amqpAdmin.declareExchange(new DirectExchange(exchange));
+        return "创建Exchange成功";
+    }
+
+    @GetMapping("/createQueue")
+    public String createQueue(@RequestParam("queue") String queue) {
+        amqpAdmin.declareQueue(new Queue(queue, true));
+        return "创建Queue成功";
+    }
+
+    @GetMapping("/createBinding")
+    public String createBinding(@RequestParam("exchange") String exchange, @RequestParam("routingKey") String routingKey) {
+        amqpAdmin.declareBinding(new Binding(routingKey, Binding.DestinationType.QUEUE, exchange, routingKey, null));
+        return "Binding成功";
     }
 
 }
